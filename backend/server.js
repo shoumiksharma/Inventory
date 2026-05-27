60b4a5fb9db20656f7dcb9daf158486c090d10fb
Shoumik Sharma <sharmashoumik@gmail.com>
2026-05-27 17:42:29 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -43,7 +43,7 @@ app.use("/api/product", productRoutes);
 
 
 app.use(express.static(path.join(__dirname, "/frontend/dist")));
-app.get('*', (req, res) => {
+app.get((req, res) => {
     res.sendFile(path.join(__dirname, 'frontend', 'dist', 'index.html'));
 });
 
